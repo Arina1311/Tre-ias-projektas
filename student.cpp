f238b9842789485b439b1afdc9d53d12a41fdace
Arina1311 <144428021+Arina1311@users.noreply.github.com>
2023-09-20 15:21:41 +0300

    Update student.cpp
    Programa atlieka veiksmus pagal pirmus reikalavimus, skaičiuoja galutinį vidurkį studento. Programa papildyta įvesties sakyniais darbui su vartotoju ir skaičiavimais. Bei išvedimo sakiniu su rezultatu.

--- student.cpp
+++ student.cpp
@@ -9,14 +9,55 @@ struct Studentas {
     string Vardas, Pavarde;
     vector<int> NamuDarbai;
     int Egzaminas;
-    float Vidurkis;
+    float Vidurkis = 0.0;
 };
 
-int main() 
-{
+int main() {
 
-    Studentas Laikinas;
+    Studentas Laikinas; 
     vector<Studentas> grupe;
     int StudentuSkaicius, NDSkaicius;
-    int NDVidurkis = 0;
+
+    cout << "Iveskite studentu skaiciu: ";
+    cin >> StudentuSkaicius;
+
+   
+    for (int i = 0; i < StudentuSkaicius; i++) {
+        
+        cout << "Iveskite varda ir pavarde studento: ";
+        cin >> Laikinas.Vardas >> Laikinas.Pavarde;
+
+        cout << "Iveskite namu darbu skaiciu: ";
+        cin >> NDSkaicius;
+
+        int NDVidurkis = 0; // Kiekvienam studentui naujas vidurkis
+
+        for (int j = 0; j < NDSkaicius; j++) {
+            int k;
+            cout << "Iveskite " << j + 1 << " pazymi: ";
+            cin >> k;
+            Laikinas.NamuDarbai.push_back(k); // Tiknka jeigu reikie perziureti namu darbu rezultatus
+            NDVidurkis += k;
+        }
+
+        NDVidurkis /= NDSkaicius; 
+
+        cout << "Iveskite egzamino pazymi: ";
+        cin >> Laikinas.Egzaminas;
+
+        Laikinas.Vidurkis = Laikinas.Egzaminas * 0.6 + NDVidurkis * 0.4;
+
+        grupe.push_back(Laikinas);
+        Laikinas.NamuDarbai.clear();
+    }
+
+    cout << fixed << setprecision(2);
+    cout << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(15) << "Galutinis (Vid.)" << endl;
+    cout << "---------------------" << "---------------------" << "---------------------" << endl;
+
+    for (auto &a : grupe) {
+        cout << left << setw(15) << a.Vardas << setw(15) << a.Pavarde << setw(15) << a.Vidurkis << endl;
+    }
+
+    return 0;
 }
