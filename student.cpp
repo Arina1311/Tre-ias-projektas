7b29885221c5b7b1b5e3d561d85338678244df82
Arina1311 <144428021+Arina1311@users.noreply.github.com>
2023-09-20 14:33:18 +0300

    Create student.cpp
    Kuriama struktūra ir kintamieji

---
+++ student.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+struct Studentas {
+    string Vardas, Pavarde;
+    vector<int> NamuDarbai;
+    int Egzaminas;
+    float Vidurkis=0.0;
+};
+
+int main() 
+{
+
+    Studentas Laikinas;
+    vector<Studentas> grupe;
+    int StudentuSkaicius, NDSkaicius;
+    int NDVidurkis = 0;
+}
