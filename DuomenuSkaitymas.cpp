e31d9e50835eddaf0a02798096e3cf47cde83bf3
Arina1311 <144428021+Arina1311@users.noreply.github.com>
2023-09-27 14:05:53 +0300

    Update DuomenuSkaitymas.cpp
    Programa papildyma klaudų gaudymo struktūra (exception handling) try - catch.

--- DuomenuSkaitymas.cpp
+++ DuomenuSkaitymas.cpp
@@ -39,6 +39,7 @@ double GalutinisMediana (double mediana, int egzaminas)
 {
     return 0.4 * mediana + 0.6 * egzaminas;
 }
+
 //-------------Palyginimas pagal pavarde--------------
 bool palygintiPagalPavarde(const Studentas& a, const Studentas& b) {
     return a.Pavarde < b.Pavarde;
@@ -48,6 +49,7 @@ bool palygintiPagalVarda(const Studentas& a, const Studentas& b) {
     return a.Vardas < b.Vardas;
 }
 //----------------------------------------------------
+
 int main() {
 
     Studentas Laikinas; 
@@ -56,21 +58,55 @@ int main() {
     char Pasirinkimas;
 
     char parinktis;
-    cout <<"Iveskite raide (R), jei norite rašyti duomenis savarankiškai ir (F) jeigu norite nuskaityti iš failo: ";
-    cin >> parinktis;
+    bool tinkamaParinktis = false; // Sukuriame kintamąjį, kuris žymės, ar parinktis tinkama
+
+    while (!tinkamaParinktis) {
+        cout << "Iveskite raide (R), jei norite rašyti duomenis savarankiškai ir (F) jeigu norite nuskaityti iš failo: ";
+    try {
+        cin >> parinktis;
+        if (parinktis != 'R' && parinktis != 'r' && parinktis != 'F' && parinktis != 'f') {
+            throw invalid_argument("Netinkama parinktis. Įveskite 'R' arba 'F'.");
+        }
+        tinkamaParinktis = true; // Jei pasiekėme šią eilutę, tai parinktis yra tinkama
+    } catch (const invalid_argument &e) {
+        cerr << e.what() << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Valome įvestį
+    }
+    }
 
     //pasirinkimas is failo ar ranka
     if (parinktis == 'R' || parinktis == 'r'){
-    
+        try {
         cout << "Iveskite studentu skaiciu: ";
         cin >> StudentuSkaicius;
 
+            if (cin.fail() || StudentuSkaicius <= 0) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            throw invalid_argument("Netinkamas studentu skaicius. Prašome įvesti teigiama sveikąjį skaičių.");
+            }
+        }   
+        catch (const invalid_argument &e) {
+            cerr << e.what() << endl;
+            return 1;
+        }
+
         char atsakymas;
         cout << "Pasirinkite ar norite patys irasyti namu darbus (S) ar sugeneruoti (A)?";
+        try {
         cin >> atsakymas;
+            if (atsakymas != 'S' && atsakymas != 's' && atsakymas != 'A' && atsakymas != 'a') {
+                throw invalid_argument("Netinkama parinktis. Pasirinkite 'S' arba 'A'.");
+            }
+        } 
+        catch (const invalid_argument &e) {
+            cerr << e.what() << endl;
+            return 1; 
+        }
 
         //Pasirinkimas sugeneruoti ar ivesti
-        if (atsakymas == 'A')
+        if (atsakymas == 'A' || atsakymas == 'a')
         {
             for (int i = 0; i < StudentuSkaicius; i++) 
             {   
@@ -79,7 +115,6 @@ int main() {
 
             Laikinas.Vidurkis = 0.0;
             Laikinas.Mediana = 0.0;
-
             NDSkaicius = 0;
 
             // Generuojame atsitiktinius namų darbų balus
@@ -115,41 +150,45 @@ int main() {
         {
             for (int i = 0; i < StudentuSkaicius; i++) 
             {
-                cout << "Iveskite varda ir pavarde studento: ";
+            cout << "Iveskite varda ir pavarde studento: ";
             cin >> Laikinas.Vardas >> Laikinas.Pavarde;
 
             Laikinas.Vidurkis = 0.0;
             Laikinas.Mediana = 0.0;
-
             NDSkaicius = 0;
 
             cin.ignore();
 
-            while (true) 
-            { 
-                cout << "Iveskite namu darbo pazymi arba pastauskite ENTER du kartus, kad pabaigtumete ivedima: ";
-                string NDpaz;
-                getline(cin, NDpaz);
+            while (true) {
+            cout << "Iveskite namu darbo pazymi arba pastauskite ENTER du kartus, kad pabaigtumete ivedima: ";
+            string NDpaz;
+            getline(cin, NDpaz);
 
+            try {
                 if (NDpaz.empty()) {
                     if (NDSkaicius == 0) {
-                        cerr << "Klaida. Reikalingas bent vienas namu darbas." << endl;
-                        continue;
+                        throw invalid_argument("Klaida. Reikalingas bent vienas namu darbas.");
                     }
                     break;
-                }   
-
-                if (cin.fail()) {
-                    cerr << "Klaida: Netinkama ivestis. Prašome ivesti skaiciu." << endl;
-                    cin.clear();
-                    continue;
                 }
 
-                int rezultatas = stoi(NDpaz);
-                Laikinas.NamuDarbai.push_back(rezultatas);
-                NDSuma += rezultatas;
+                int pazymys = stoi(NDpaz); //konvertuojama i int skaiciu
+
+                if (cin.fail()) {
+                    throw invalid_argument("Klaida: Netinkama ivestis. Prašome ivesti skaiciu.");
+                }
+
+                Laikinas.NamuDarbai.push_back(pazymys);
+                NDSuma += pazymys;
                 NDSkaicius++;
+
+            } catch (const invalid_argument &e) {
+                cerr << e.what() << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
             }
+        }
+
             cout << "Iveskite egzamino pazymi: ";
             cin >> Laikinas.Egzaminas;
 
@@ -165,68 +204,83 @@ int main() {
             }
         }
     }
-    else {
-        string FileName;
-        cout << "Enter the name of the input file: ";
-        cin >> FileName;
 
-    ifstream input(FileName); // Open the input file
-    if (!input.is_open()) {
-        cerr << "Nepavyko atidaryti failo." << endl;
-        return 1;
-    }
-        // Read the header line (column names)
+    else{
+    string FileName;
+    cout << "Iveskite failo pavadinima: ";
+    cin >> FileName;
+
+    ifstream input(FileName); // Bandom atidaryti failą
+
+    try {
+        if (!input.is_open()) {
+            throw runtime_error("Nepavyko atidaryti failo."); // Sukeliame išimtį, jei atidarymas nepavyksta
+        }
+
+        // Skaitome pirma eilute su pavadinimais
         string header;
         int n;
-        if (getline(input, header)) {
-            istringstream iss(header);
-            vector<string> stulpeliai;
-            string token;
 
-            while (iss >> token) {
-                stulpeliai.push_back(token);
+        try {
+            if (getline(input, header)) {
+                istringstream iss(header); //istringstream klasės objektas, srautas
+                vector<string> stulpeliai;
+                string pav;
+
+                while (iss >> pav) {
+                    stulpeliai.push_back(pav);
+                }
+
+                n = stulpeliai.size() - 3;
+            } else {
+                throw runtime_error("Failas yra tuščias.");
             }
-
-            n = stulpeliai.size()-3;
-        } 
-        else {
-            cerr << "The file is empty." << endl;
+        } catch (const runtime_error &e) {
+            cerr << e.what() << endl;
+            return 1; // Grąžiname klaidos kodą
         }
 
-    // Read data from the file
-    while (!input.eof()) {
-        input >> Laikinas.Vardas >> Laikinas.Pavarde;
-        for (int i = 0; i < n; i++) {
-            int pazymys;
-            input >> pazymys;
-            Laikinas.NamuDarbai.push_back(pazymys);
+        // Skaitome likusius duomenys is failo
+        while (!input.eof()) {
+            input >> Laikinas.Vardas >> Laikinas.Pavarde;
+            for (int i = 0; i < n; i++) {
+                int pazymys;
+                input >> pazymys;
+                Laikinas.NamuDarbai.push_back(pazymys);
+            }
+            input >> Laikinas.Egzaminas;
+
+            Laikinas.Vidurkis = GalutinisVidurkis(NDSuma, Laikinas.NamuDarbai, Laikinas.Egzaminas);
+
+            double TarpineMediana = Mediana(Laikinas.NamuDarbai);
+            Laikinas.Mediana = GalutinisMediana(TarpineMediana, Laikinas.Egzaminas);
+
+            grupe.push_back(Laikinas);
+            Laikinas.NamuDarbai.clear();
         }
-        input >> Laikinas.Egzaminas;
 
-        Laikinas.Vidurkis = GalutinisVidurkis(NDSuma, Laikinas.NamuDarbai, Laikinas.Egzaminas);
-
-        double TarpineMediana = Mediana(Laikinas.NamuDarbai);
-        Laikinas.Mediana = GalutinisMediana(TarpineMediana, Laikinas.Egzaminas);
-
-        grupe.push_back(Laikinas);
-        Laikinas.NamuDarbai.clear();
+        input.close();
     }
-    input.close(); 
+    catch (const runtime_error &e) {
+        cerr << e.what() << endl; // Spausdiname klaidos pranešimą
+        return 1; // Grąžiname klaidos kodą
     }
-
-    //Leidziame vartotojui pasirinkti norima skaiciavimo buda
-    cout << "Noredami apskaiciuoti vidurki iveskite (V) ar mediana (M) ";
-    cin >> Pasirinkimas;
-
-    ofstream output("rezultatai.txt"); // Create a new output file
-    if (!output.is_open()) {
-        cerr << "Failed to create the output file." << endl;
-        return 1;
     }
 
     //Palyginimas ir rusiavimas pagal pavarde ir varda
     sort(grupe.begin(), grupe.end(), palygintiPagalPavarde);
     stable_sort(grupe.begin(), grupe.end(), palygintiPagalVarda);
+
+    //Leidziame vartotojui pasirinkti norima skaiciavimo buda
+    cout << "Noredami apskaiciuoti vidurki iveskite (V) ar mediana (M) ";
+    cin >> Pasirinkimas;
+
+   ofstream output("rezultatai.txt"); // Kuriame nauja faila
+
+    try {
+    if (!output.is_open()) {
+        throw runtime_error("Nepavuko sukurti failo su rezultatais."); 
+    }
 
     output << fixed << setprecision(2);
     output << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(20) << (Pasirinkimas == 'V' ? "Galutinis(Vid.)" : "Galutinis(Med.)") << setw(15) << "Egzaminas" ;
@@ -240,8 +294,13 @@ int main() {
         output << endl;
     }
 
-    output.close(); // Close the output file
-
+    output.close(); // Uzdarome
     cout << "Rezultatai paruošti faile";
+    } 
+    
+    catch (const runtime_error &e) {
+    cerr << e.what() << endl; 
+    return 1; 
+    }
     return 0;
 }
