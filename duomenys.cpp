3e058b3d5c9c3cc59e0e9ab75f5b67472913f7c9
Arina1311 <144428021+Arina1311@users.noreply.github.com>
2023-10-09 21:45:01 +0300

    Update duomenys.cpp
    Pridetos papildomos funkcijos testavimui.

--- duomenys.cpp
+++ duomenys.cpp
@@ -2,6 +2,8 @@
 #include "duomenys.h"
 #include <algorithm>
 
+
+
 float GalutinisVidurkis(double suma, vector<int> duomenys, int egzaminas) {
     int kiekis = duomenys.size();
     return 0.4 * (suma / kiekis) + 0.6 * egzaminas;
@@ -28,3 +30,41 @@ bool palygintiPagalPavarde(const Studentas &a, const Studentas &b) {
 bool palygintiPagalVarda(const Studentas &a, const Studentas &b) {
     return a.Vardas < b.Vardas;
 }
+
+/*double Tarpinis (double M[], int k)
+{
+    double geriausias = M[0];
+    for (int i = 1; i < k; i++) {
+        if (M[i] < geriausias) {
+            geriausias = M[i];
+        }
+    }
+    return geriausias;
+}
+*/
+
+double vidutiniai (double M[], int k)
+{
+    double sumav=0;
+    for (int i = 0; i < k; i++) {
+        sumav += M[i];
+    }
+    double vid = sumav / k;
+    return vid;
+}
+
+double finalinis(double A[], double B[], double C[], double D[], double E[], int testavimask) {
+    double F[testavimask];
+    for (int i = 0; i < testavimask; i++) {
+        F[i] = A[i] + B[i] + C[i] + D[i] + E[i];
+    }
+
+    double suma = 0;
+    for (int k = 0; k < testavimask; k++) {
+        suma += F[k];
+    }
+
+    double vidurkis = suma / testavimask;
+    return vidurkis;
+}
+
