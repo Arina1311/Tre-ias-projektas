40c39bb53111f3c67030f6b5b83352e355e4d01c
Arina1311 <144428021+Arina1311@users.noreply.github.com>
2023-11-03 16:40:54 +0200

    Update duomenys.h
    Prideta nauja funkcija

--- duomenys.h
+++ duomenys.h
@@ -3,22 +3,23 @@
 #define DUOMENYS_H
 
 #include <string>
-#include <list>
+#include <vector>
 using namespace std;
 
 struct Studentas {
     string Vardas, Pavarde;
-    list<int> NamuDarbai;
+    vector<int> NamuDarbai;
     int Egzaminas;
     float Pazymys;
 };
 
 
-float GalutinisPazymis(list<int>& duomenys, int egzaminas, char pasirinkimas);
+float GalutinisPazymis(double suma, vector<int>& duomenys, int egzaminas, char pasirinkimas);
 bool palygintiPagalVarda(const Studentas &a, const Studentas &b);
 bool palygintiPagalPavarde(const Studentas &a, const Studentas &b);
 bool palygintiPagalPazymi(const Studentas &a, const Studentas &b);
 double vidutiniai (double M[], int k);
 double finalinis (double A[], double B[], double C[], double D[], double E[], int testavimask);
+bool isVargsiukas(const Studentas &studentas);
 
 #endif 
